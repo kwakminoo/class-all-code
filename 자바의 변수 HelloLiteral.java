f53d3a322892fd7690fd2b6c80f88fd21de7ab64
kwakminoo <105197503+kwakminoo@users.noreply.github.com>
2022-09-26 16:10:44 +0900

    Create 자바의 변수 HelloLiteral.java

---
+++ 자바의 변수 HelloLiteral.java
@@ -0,0 +1,23 @@
+package programming.basic;
+
+public class HelloLiteral {
+	
+	static int def;
+	static boolean bool;
+	static String name;
+	
+	public static void main(String[] args) {
+		
+		int n = 100;
+		System.out.println(n);
+		
+		System.out.println(def);
+		System.out.println(bool);
+		System.out.println(name);
+
+
+		
+		
+	}
+
+}
